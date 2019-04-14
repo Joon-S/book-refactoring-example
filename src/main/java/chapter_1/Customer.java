bd450fa89be46ae413b15c5010690e303612cf3f
Joon-S <niceweather94@gmail.com>
2019-04-14 16:39:33 +0900

    Initial commit

---
+++ src/main/java/chapter_1/Customer.java
@@ -0,0 +1,20 @@
+package chapter_1;
+
+import java.util.Vector;
+
+public class Customer {
+    private String _name;
+    private Vector _rentals = new Vector();
+
+    public Customer(String _name) {
+        this._name = _name;
+    }
+
+    public void addRental(Rental arg) {
+        _rentals.addElement(arg);
+    }
+
+    public String get_name() {
+        return _name;
+    }
+}
---
+++ src/main/java/chapter_1/Movie.java
@@ -0,0 +1,26 @@
+package chapter_1;
+
+public class Movie {
+    public static final int CHILDRENS = 2;
+    public static final int REGULAR = 0;
+    public static final int NEW_RELEASE = 1;
+    private String _title;
+    private int _priceCode;
+
+    public Movie(String _title, int _priceCode) {
+        this._title = _title;
+        this._priceCode = _priceCode;
+    }
+
+    public String get_title() {
+        return _title;
+    }
+
+    public int get_priceCode() {
+        return _priceCode;
+    }
+
+    public void set_priceCode(int _priceCode) {
+        this._priceCode = _priceCode;
+    }
+}
